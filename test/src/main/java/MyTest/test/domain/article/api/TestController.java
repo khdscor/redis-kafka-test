473e872aedeeb53aa32f1746357b7d0eebeb26f7
khdscor <khds_cor@naver.com>
2023-09-18 23:58:16 +0900

    feat: redis 기초 실습

---
+++ test/src/main/java/MyTest/test/domain/article/api/TestController.java
@@ -0,0 +1,30 @@
+package MyTest.test.domain.article.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name){
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("fruit", name);
+
+        return "saved.";
+    }
+
+    @GetMapping("/getFruit")
+    public String getFruit(){
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+
+        return ops.get("fruit");
+    }
+}
